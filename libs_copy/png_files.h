d5c39080e15dc451bc281eba18e77670bda82bcb
macbook <rodolfolabiapari@gmail.com>
2017-08-31 14:04:35 -0300

    remove arquivos salvos

--- libs_copy/png_files.h
+++
@@ -1,8 +0,0 @@
-
-#include <png.h>
-void read_png_file(int * width, int * height,
-png_byte * color_type, png_byte * bit_depth, png_bytep **row_pointers);
-
-
-void copy_png_to_float(int height, int width, png_bytep *row_pointers,
-  float *** data);
